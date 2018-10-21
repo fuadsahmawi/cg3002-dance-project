1470855db3af418ae7d027904830ef49b192273a
Melvin Tan <melvintan123@gmail.com>
2018-10-21 18:24:06 +0800

    change plotting style

--- Pi/visualisation.py
+++ Pi/visualisation.py
@@ -10,24 +10,45 @@ df = read_csv(sys.argv[1], header=0, index_col=None)
 ## plot data per person
 window = range(0, df.shape[0])
 
-g_x = df.values[window, 0]
-pyplot.plot(window, g_x)
+pyplot.subplot(2,1,1)
 
-g_y = df.values[window, 1]
-pyplot.plot(window, g_y)
+g1_x = df.values[window, 0]
+pyplot.plot(window, g1_x)
 
-g_z = df.values[window, 2]
-pyplot.plot(window, g_z)
+g1_y = df.values[window, 1]
+pyplot.plot(window, g1_y)
 
-acc_x = df.values[window, 3]
-pyplot.plot(window, acc_x)
+g1_z = df.values[window, 2]
+pyplot.plot(window, g1_z)
 
-acc_y = df.values[window, 4]
-pyplot.plot(window, acc_y)
+acc1_x = df.values[window, 3]
+pyplot.plot(window, acc1_x)
 
-acc_z = df.values[window, 5]
-pyplot.plot(window, acc_z)
+acc1_y = df.values[window, 4]
+pyplot.plot(window, acc1_y)
+
+acc1_z = df.values[window, 5]
+pyplot.plot(window, acc1_z)
 
 
+pyplot.subplot(2,1,1)
+
+g2_x = df.values[window, 6]
+pyplot.plot(window, g2_x)
+
+g2_y = df.values[window, 7]
+pyplot.plot(window, g2_y)
+
+g2_z = df.values[window, 8]
+pyplot.plot(window, g2_z)
+
+acc2_x = df.values[window, 9]
+pyplot.plot(window, acc2_x)
+
+acc2_y = df.values[window, 10]
+pyplot.plot(window, acc2_y)
+
+acc2_z = df.values[window, 11]
+pyplot.plot(window, acc2_z)
 
 pyplot.show()
